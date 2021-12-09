80a500edc26bf5b9622b8511ae30f2d9f693ef50
kathyswu <kathy.s.wu@hotmail.com>
2021-12-08 23:42:09 -0800

    Fix JS loading

--- scripts/app.js
+++ scripts/app.js
@@ -43,7 +43,7 @@ TxtType.prototype.tick = function () {
   }, delta);
 };
 
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
   var elements = document.getElementsByClassName("typewrite");
   for (var i = 0; i < elements.length; i++) {
     var toRotate = elements[i].getAttribute("data-type");
@@ -57,4 +57,20 @@ window.onload = function () {
   css.type = "text/css";
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #71c19d}";
   document.body.appendChild(css);
-};
+});
+
+// window.onload = function () {
+//   var elements = document.getElementsByClassName("typewrite");
+//   for (var i = 0; i < elements.length; i++) {
+//     var toRotate = elements[i].getAttribute("data-type");
+//     var period = elements[i].getAttribute("data-period");
+//     if (toRotate) {
+//       new TxtType(elements[i], JSON.parse(toRotate), period);
+//     }
+//   }
+//   // INJECT CSS
+//   var css = document.createElement("style");
+//   css.type = "text/css";
+//   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #71c19d}";
+//   document.body.appendChild(css);
+// };
