fa2a284fc52b8a3c9f8615b0da43ac1423f310f5
kathyswu <kathy.s.wu@hotmail.com>
2021-12-08 23:55:54 -0800

    Fix resume and js

--- scripts/app.js
+++ scripts/app.js
@@ -58,19 +58,3 @@ document.addEventListener("DOMContentLoaded", function () {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #71c19d}";
   document.body.appendChild(css);
 });
-
-// window.onload = function () {
-//   var elements = document.getElementsByClassName("typewrite");
-//   for (var i = 0; i < elements.length; i++) {
-//     var toRotate = elements[i].getAttribute("data-type");
-//     var period = elements[i].getAttribute("data-period");
-//     if (toRotate) {
-//       new TxtType(elements[i], JSON.parse(toRotate), period);
-//     }
-//   }
-//   // INJECT CSS
-//   var css = document.createElement("style");
-//   css.type = "text/css";
-//   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #71c19d}";
-//   document.body.appendChild(css);
-// };
